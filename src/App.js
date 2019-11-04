ce88601e6b49794fa59c84101e9d794fd7ce7665
Ida Ferm <ida.kg.ferm@gmail.com>
2019-11-04 12:27:50 +0100

    Added Header component and all-round styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.scss';
+import Header from './components/Header/Header';
 import Flap from './components/Flap/Flap';
 import calendarData from './calendar-data.json';
 
 function App() {
 	return (
 		<div className="App">
-			{calendarData.map(item => {
+      <Header />
+      <div className="FlapWrapper">
+      {calendarData.map(item => {
 				return <Flap key={item.day} day={item.day} backgroundColor={item.backgroundColor} text={item.text} />;
-			})}
+      })}
+      </div>
 		</div>
 	);
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+	return (
+    <div className="Header">
+		<h1>Creative Calender</h1>
+		<h3>Have some Christmas fun!</h3>
+    </div>
+    );
+};
+    
+export default Header;
